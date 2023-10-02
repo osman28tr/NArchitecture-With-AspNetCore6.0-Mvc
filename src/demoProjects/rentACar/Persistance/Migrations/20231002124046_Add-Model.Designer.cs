56b19617a16199ead6f9bae88ffc0f3f76e829f1
osman28tr <osmantonbul2001@gmail.com>
2023-10-02 15:43:29 +0300

    Model tablosu oluşturuldu ve brand ile ilişkisi yapılandırıldı.

--- src/demoProjects/rentACar/Persistance/Migrations/20231002124046_Add-Model.Designer.cs
+++ src/demoProjects/rentACar/Persistance/Migrations/20231002124046_Add-Model.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contexts;
 
@@ -10,9 +11,11 @@ using Persistence.Contexts;
 namespace Persistance.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    partial class BaseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231002124046_Add-Model")]
+    partial class AddModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
