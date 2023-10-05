d3a38dd50bf4e0705dd8e29ba59dd6357310f293
osman28tr <osmantonbul2001@gmail.com>
2023-10-05 16:35:42 +0300

    ISomeFeature hatası giderildi ve dinamik sorgu yapısı kuruldu.

--- src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -18,7 +18,7 @@ namespace Application
 		{
 
 			services.AddAutoMapper(Assembly.GetExecutingAssembly());
-			services.AddMediatR(Assembly.GetExecutingAssembly());
+			services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
 			services.AddScoped<BrandBusinessRules>();
 
---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetListModel;
+using Application.Features.Models.Queries.GetListModelByDynamic;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ModelsController : BaseController
+	{
+		[HttpGet]
+		public async Task<ActionResult> GetList([FromQuery]PageRequest pageRequest)
+		{
+			GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest };
+			ModelListModel modelListModel = await Mediator.Send(getListModelQuery);
+			return Ok(modelListModel);
+		}
+		[HttpPost("GetList/ByDynamic")]
+		public async Task<ActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest,[FromBody] Dynamic dynamic)
+		{
+			GetListModelByDynamicQuery getListByDynamicModelQuery = new GetListModelByDynamicQuery { PageRequest = pageRequest, Dynamic = dynamic };
+			ModelListModel modelListModel = await Mediator.Send(getListByDynamicModelQuery);
+			return Ok(modelListModel);
+		}
+	}
+}
